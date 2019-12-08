d2e2ab617237508d7c204db1050c37d80682b131
Scott Guilbeaux <scott@herp-keeper.com>
2019-12-08 11:58:25 -0600

    Generate Angular application.

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('herp-keeper-root .content span')).getText() as Promise<string>;
+  }
+}
